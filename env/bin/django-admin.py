c12ad29051696f68b6b99e463e4869df95b39bff
BRUTCHSAMAJEANLOUIS <brutchsama14@gmail.com>
2020-12-23 00:16:06 +0000

    commit

--- env/bin/django-admin.py
+++ env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/ubuntu/django_project/env/bin/python3
+#!/home/ubuntu/django_project/env/bin/python
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
--- project/project/settings.py
+++ project/project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
 'default': {
 	'ENGINE': 'django.db.backends.mysql',
 	'NAME': 'django',
-	'USER': 'django',
+	'USER': 'root',
 	'PASSWORD': 'Password123!',
 	'HOST': '127.0.0.1',
 	'PORT': '3306',
